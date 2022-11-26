c643408503e61fe818e7bd44a5e87d57cc276ac0
mcjraquel <mcjraquel@gmail.com>
2022-11-26 22:42:13 +0800

    feat: Create Transaction class

---
+++ lib/transaction.dart
@@ -0,0 +1,17 @@
+class Transaction {
+  final String id;
+  final String type;
+  final String purpose;
+  final String bin;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    this.id,
+    this.type,
+    this.bin,
+    this.purpose,
+    this.amount,
+    this.date,
+  });
+}
