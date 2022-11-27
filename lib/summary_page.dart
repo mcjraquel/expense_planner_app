9bc92b2426d1ec2c7df4bbd5ec6ef19090c189a8
mcjraquel <mcjraquel@gmail.com>
2022-11-27 17:37:45 +0800

    fix: Make SummaryPage a StatefulWidget

--- lib/summary_page.dart
+++ lib/summary_page.dart
@@ -3,19 +3,23 @@ import 'package:flutter/material.dart';
 import 'models/bin.dart';
 import 'widgets/bin_card.dart';
 
-class SummaryPage extends StatelessWidget {
+class SummaryPage extends StatefulWidget {
   final List<Bin> bins;
 
-  const SummaryPage(this.bins);
+  const SummaryPage({super.key, required this.bins});
 
+  @override
+  State<SummaryPage> createState() => _SummaryPageState();
+}
+
+class _SummaryPageState extends State<SummaryPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Container(
       padding: const EdgeInsets.only(top: 15, bottom: 15),
       child: Column(children: <Widget>[
-        AddNewBinCard(),
-        ...bins.map((bin) => BinCard(bin)).toList(),
+        ...widget.bins.map((bin) => BinCard(bin: bin)).toList(),
       ]),
     ));
   }
