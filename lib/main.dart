a16ccb425ac0ffa4b07c4007a8c299129a48db44
mcjraquel <mcjraquel@gmail.com>
2023-02-26 23:09:11 +0800

    chore: Remove unnecessary variables and functions for bins and transactions

--- lib/main.dart
+++ lib/main.dart
@@ -39,57 +39,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MainState extends State<MyHomePage> {
-  final List<Bin> bins = [
-    Bin(id: '1', type: 'Inflow', name: 'Emergency Fund', total: 8000),
-    Bin(id: '2', type: 'Outflow', name: 'Trip Savings', total: 5000)
-  ];
-  final List<Transaction> transactions = [
-    Transaction(
-        id: '1',
-        type: 'Inflow',
-        bin: '1',
-        purpose: 'basta',
-        amount: 500,
-        date: DateTime.now())
-  ];
-
   var _currentTab = 'summary';
 
   void _changeTab(String tab) {
     setState(() {
       _currentTab = tab;
     });
-  }
-
-  void addNewBintoBinsList(String newBinName, String newBinType) {
-    final newBin = Bin(id: '3', name: newBinName, type: newBinType, total: 0);
-    setState(() {
-      bins.add(newBin);
-    });
-  }
-
-  void addNewTransactiontoTransactionsList(String newTxType,
-      String newTxPurpose, String newTxBin, double newTxAmount) {
-    newTxAmount = newTxType == 'Inflow' ? newTxAmount : -newTxAmount;
-    final newTx = Transaction(
-        id: '2',
-        type: newTxType,
-        purpose: newTxPurpose,
-        bin: newTxBin,
-        amount: newTxAmount,
-        date: DateTime.now());
-    setState(() {
-      transactions.add(newTx);
-    });
-    updateBinTotal(newTxBin, newTxAmount);
-  }
-
-  void updateBinTotal(String binToUpdate, double amount) {
-    var binObj = bins.singleWhere((bin) => bin.id == binToUpdate);
-    var binTransactions = transactions.where((tx) => tx.bin == binObj.id);
-    double binTotal =
-        binTransactions.map((tx) => tx.amount).reduce((sum, amt) => sum + amt);
-    binObj.total = binTotal;
   }
 
   @override
